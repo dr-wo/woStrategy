9d3145fca203f884e2d4305f5d7053d8d8e9eea1
dr-wo <woboshi2025@gmail.com>
2026-07-27 09:17:09 +0800

    Use shared FastF1 team colours

--- src/wostrategy/plots/style_maps.py
+++ src/wostrategy/plots/style_maps.py
@@ -3,24 +3,7 @@ from __future__ import annotations
 import matplotlib.pyplot as plt
 import pandas as pd
 
-F1_TEAM_COLORS = {
-    "Ferrari": "#dc0000",
-    "McLaren": "#ff8700",
-    "Red Bull Racing": "#3029ed",
-    "Red Bull": "#3029ed",
-    "Mercedes": "#00fbe2",
-    "Aston Martin": "#006f62",
-    "Alpine": "#ff87bc",
-    "Williams": "#1f6af4ff",
-    "Haas F1 Team": "#6e6e6e",
-    "Haas": "#6e6e6e",
-    "Cadillac": "#82d8f4",
-    "Kick Sauber": "#00e701",
-    "Sauber": "#00e701",
-    "Audi": "#000000",
-    "Racing Bulls": "#5e59ee",
-    "RB": "#5e59ee",
-}
+from wodata.colours import F1_TEAM_COLORS
 
 DRIVER_LINESTYLES_BY_RANK = {
     0: "-",
